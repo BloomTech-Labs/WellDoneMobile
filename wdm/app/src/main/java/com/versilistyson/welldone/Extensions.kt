5c712a28c43e112f73775b3fe196764f13ef598d
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-12-14 02:51:11 +0800

    added package structure

--- wdm/app/src/main/java/com/versilistyson/welldone/Extensions.kt
+++
@@ -1,4 +0,0 @@
-package com.versilistyson.welldone
-
-class Extensions {
-}
---
+++ wdm/app/src/main/java/com/versilistyson/welldone/util/Resource.kt
@@ -0,0 +1,7 @@
+package com.versilistyson.welldone.util
+
+sealed class Resource<out T> {
+    class Loading<out T> : Resource<T>()
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Failure<out T>(val throwable: Throwable) : Resource<T>()
+}
