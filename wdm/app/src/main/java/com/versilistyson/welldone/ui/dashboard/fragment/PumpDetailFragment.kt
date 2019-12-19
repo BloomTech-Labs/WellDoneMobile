c2680dea1c39522004cced7d7b295963024fd5d5
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-12-20 04:21:10 +0800

    added data binding to the detail fragment screen

--- wdm/app/src/main/java/com/versilistyson/welldone/ui/dashboard/fragment/PumpDetailFragment.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/ui/dashboard/fragment/PumpDetailFragment.kt
@@ -1,23 +1,40 @@
 package com.versilistyson.welldone.ui.dashboard.fragment
 
 
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 import com.versilistyson.welldone.R
+import com.versilistyson.welldone.databinding.FragmentPumpDetailBinding
+import com.versilistyson.welldone.ui.dashboard.DashboardViewmodel
 
 /**
  * A simple [Fragment] subclass.
  */
 class PumpDetailFragment : Fragment() {
 
+    private lateinit var viewmodel: DashboardViewmodel
+    private lateinit var binding: FragmentPumpDetailBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pump_detail, container, false)
+        viewmodel = activity.let {
+            val appContext = activity?.applicationContext as Application
+            ViewModelProvider
+                .AndroidViewModelFactory
+                .getInstance(appContext)
+                .create(DashboardViewmodel::class.java)
+        }
+
+        binding = FragmentPumpDetailBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = this@PumpDetailFragment
+            pump = viewmodel.selectedMarkerSensor!!
+        }
+        return binding.root
     }
-
-
 }
