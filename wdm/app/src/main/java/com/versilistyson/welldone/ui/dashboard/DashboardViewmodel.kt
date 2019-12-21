269ad45839ddf77efc80dbf58c7750373f6c8fef
GIANCARLO GATTI <gattigcg1@gmail.com>
2019-12-21 18:19:34 +0800

    change map to use MapBox api

--- wdm/app/src/main/java/com/versilistyson/welldone/ui/dashboard/DashboardViewmodel.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/ui/dashboard/DashboardViewmodel.kt
@@ -1,18 +1,13 @@
 package com.versilistyson.welldone.ui.dashboard
 
 import android.app.Application
-import androidx.lifecycle.*
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.versilistyson.MyApplication
-import com.versilistyson.welldone.R
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.liveData
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.versilistyson.welldone.data.remote.dto.SensorRecentResponse
 import com.versilistyson.welldone.repository.DashboardRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class DashboardViewmodel(application: Application): AndroidViewModel(application) {
 
@@ -44,34 +39,34 @@ class DashboardViewmodel(application: Application): AndroidViewModel(application
         }
     }
 
-    fun clickedOnPumpMarker(marker: Marker){
-        selectedMarkerSensor = marker.tag as SensorRecentResponse
-    }
-
-    fun getAverageLatitudeLongitude(sensors: List<SensorRecentResponse>, mMap: GoogleMap){
-
-        viewModelScope.launch(Dispatchers.Main) {
-            var totalLat = 0.0
-            var totalLng = 0.0
-
-            for (sensor in sensors) {
-                val point = LatLng(sensor.latitude, sensor.longitude)
-                totalLat += point.latitude
-                totalLng += point.longitude
-
-                val marker = MarkerOptions()
-                    .position(point)
-
-                when (sensor.status) {
-                    null -> marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.no_data_marker))
-                    1 -> marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.non_working_marker))
-                    2 -> marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.working_marker))
-                }
-
-                mMap.addMarker(marker).tag = sensor
-            }
-
-            _averageLatitudeLongitudeLiveData.postValue(LatLng(totalLat, totalLng))
-        }
-    }
+//    fun clickedOnPumpMarker(marker: Marker){
+//        selectedMarkerSensor = marker.tag as SensorRecentResponse
+//    }
+//
+//    fun getAverageLatitudeLongitude(sensors: List<SensorRecentResponse>, mMap: MapboxMap){
+//
+//        viewModelScope.launch(Dispatchers.Main) {
+//            var totalLat = 0.0
+//            var totalLng = 0.0
+//
+//            for (sensor in sensors) {
+//                val point = LatLng(sensor.latitude, sensor.longitude)
+//                totalLat += point.latitude
+//                totalLng += point.longitude
+//
+//                val marker = MarkerOptions()
+//                    .position(point)
+//
+//                when (sensor.status) {
+//                    null -> marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.no_data_marker))
+//                    1 -> marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.non_working_marker))
+//                    2 -> marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.working_marker))
+//                }
+//
+//                mMap.addMarker(marker).tag = sensor
+//            }
+//
+//            _averageLatitudeLongitudeLiveData.postValue(LatLng(totalLat, totalLng))
+//        }
+//    }
 }
