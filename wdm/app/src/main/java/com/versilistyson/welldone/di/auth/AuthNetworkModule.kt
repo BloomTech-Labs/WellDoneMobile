ca4f14c922fc0b138694aded1f1e9105028a3f08
GIANCARLO GATTI <gattigcg1@gmail.com>
2020-01-15 06:05:03 +0800

    dagger implementation successful

--- wdm/app/src/main/java/com/versilistyson/welldone/di/auth/AuthNetworkModule.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/di/auth/AuthNetworkModule.kt
@@ -9,7 +9,6 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
 
 @Module
 object AuthNetworkModule {
