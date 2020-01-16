c6173b5e184bd81dd62337198d1fea8b9397139f
GIANCARLO GATTI <gattigcg1@gmail.com>
2020-01-16 17:19:18 +0800

    first

--- wdm/app/src/main/java/com/versilistyson/welldone/data/datasource/user/IUserApiDataSource.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/datasource/user/IUserApiDataSource.kt
@@ -1,8 +1,7 @@
 package com.versilistyson.welldone.data.datasource.user
 
-import com.versilistyson.welldone.data.datasource.BaseDataSource
 import com.versilistyson.welldone.domain.entity.Entity
 
-interface IUserApiDataSource : BaseDataSource {
+interface IUserApiDataSource {
     suspend fun signInUser(email: String, password: String) : Entity.AuthenticatedUser
 }
