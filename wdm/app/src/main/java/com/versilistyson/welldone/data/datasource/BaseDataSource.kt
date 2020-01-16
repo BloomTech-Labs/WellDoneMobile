1b02f976d417c437ef023f1928463b92cc468c9e
GIANCARLO GATTI <gattigcg1@gmail.com>
2020-01-17 04:31:02 +0800

    w

--- wdm/app/src/main/java/com/versilistyson/welldone/data/datasource/BaseDataSource.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/datasource/BaseDataSource.kt
@@ -1,11 +1,13 @@
 package com.versilistyson.welldone.data.datasource
 
+import androidx.lifecycle.LiveData
 import com.versilistyson.welldone.domain.common.Result
 import retrofit2.Response
 import java.lang.Exception
 
 abstract class BaseDataSource {
-    protected suspend fun <T> getResult(call: suspend () -> Response<T>) : Result<T> {
+
+    protected suspend fun <T> getResultByNetwork(call: suspend () -> Response<T>) : Result<T> {
         try {
             val response = call()
             if(response.isSuccessful) {
@@ -17,4 +19,10 @@ abstract class BaseDataSource {
             return Result.error(e.message ?: e.toString())
         }
     }
+
+    abstract fun <T> getResultByDatabase(call: suspend() -> LiveData<T>): LiveData<Result<T>>
+
+    abstract fun <T> saveResultToDatabase(call: suspend(T) -> Result<T>)
+
+
 }
