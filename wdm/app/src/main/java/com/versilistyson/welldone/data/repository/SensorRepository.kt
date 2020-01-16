37808d35672cd715f35adcf30d6312e75d683198
vdtyson <versilistyson@gmail.com>
2020-01-16 16:32:22 -0500

    - Adds sensordatasource to SensorRepository

--- wdm/app/src/main/java/com/versilistyson/welldone/data/repository/SensorRepository.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/repository/SensorRepository.kt
@@ -1,11 +1,11 @@
 package com.versilistyson.welldone.data.repository
 
+import com.versilistyson.welldone.data.datasource.sensor.SensorDataSource
 import com.versilistyson.welldone.domain.common.Result
 import com.versilistyson.welldone.domain.entity.Entity
 import com.versilistyson.welldone.domain.ISensorRepository
 
-class SensorRepository(
-) : ISensorRepository {
+class SensorRepository(dataSource: SensorDataSource) : ISensorRepository {
     override suspend fun fetchAllSensors(): Result<List<Entity.Sensor>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
