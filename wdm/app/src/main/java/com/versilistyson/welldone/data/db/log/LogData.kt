6ecc9fc1e7e5ee12eec4205d34e06be06dd2e315
vdtyson <versilistyson@gmail.com>
2020-01-21 14:29:07 -0500

    - Adds functions to daos

--- wdm/app/src/main/java/com/versilistyson/welldone/data/db/log/LogData.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/db/log/LogData.kt
@@ -5,24 +5,22 @@ import androidx.room.*
 import com.mapbox.mapboxsdk.style.expressions.Expression.array
 import com.versilistyson.welldone.data.db.sensor.SensorData
 
-sealed class LogData {
-    @Entity(
-        tableName = "log_table",
-        foreignKeys = [ForeignKey(
-            entity = SensorData.Sensor::class,
-            parentColumns = ["id"],
-            childColumns = ["sensor_id"],
-            onDelete = ForeignKey.CASCADE
-        )],
-        indices = [Index(value = ["remote_id"], unique = true)]
-    )
-    data class OperatorLog(
-        @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: Long,
-        @ColumnInfo(name = "remote_id") val remoteId: Long,
-        @ColumnInfo(name = "sensor_id") val sensorId: Long,
-        @ColumnInfo(name = "date_filed") val dateFiled: String,
-        @ColumnInfo(name = "last_modified") val lastModified: String,
-        @ColumnInfo(name = "status") val status: Drawable,
-        @ColumnInfo(name = "comment") val comment: String
-    ) : LogData()
-}
+@Entity(
+    tableName = "log_table",
+    foreignKeys = [ForeignKey(
+        entity = SensorData::class,
+        parentColumns = ["remote_id"],
+        childColumns = ["remote_sensor_id"],
+        onDelete = ForeignKey.CASCADE
+    )],
+    indices = [Index(value = ["remote_id"], unique = true)]
+)
+data class LogData(
+    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "remote_id") val remoteId: Long,
+    @ColumnInfo(name = "remote_sensor_id") val remoteSensorId: Long,
+    @ColumnInfo(name = "date_filed") val dateFiled: String,
+    @ColumnInfo(name = "last_modified") val lastModified: String,
+    @ColumnInfo(name = "status") val status: Drawable,
+    @ColumnInfo(name = "comment") val comment: String
+)
