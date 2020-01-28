2f50f0685a23d8dda1fee641faca4777cc0504cc
vdtyson <versilistyson@gmail.com>
2020-01-28 18:57:17 -0500

    - Adds UserAuthenticationLocalDataSourceImpl and interface - Adds SharedPreferenceKeys file

--- wdm/app/src/main/java/com/versilistyson/welldone/domain/framework/datasource/user/AuthenticationRemoteDataSource.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/domain/framework/datasource/user/AuthenticationRemoteDataSource.kt
@@ -2,4 +2,5 @@ package com.versilistyson.welldone.domain.framework.datasource.user
 
 interface AuthenticationRemoteDataSource {
     fun signIn(email: String, password: String)
+    fun isTokenValid(token: String): Boolean
 }
