c0c7d9e9c275fa0e14a7f7738e60c302df4af4a8
GIANCARLO GATTI <gattigcg1@gmail.com>
2020-01-29 01:38:14 +0800

    fixes to sensor dao

--- wdm/app/src/main/java/com/versilistyson/welldone/data/db/sensor/SensorDao.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/db/sensor/SensorDao.kt
@@ -10,11 +10,11 @@ interface SensorDao {
     suspend fun saveAll(key: String, sensors: List<SensorData>)
 
     @Delete
-    fun deleteAll(key: (List<SensorData>) -> List<Int>)
+    fun deleteAll(key: String)
 
     @Query("SELECT * FROM sensor_table")
     fun getAll(key: String) : Flow<List<SensorData>>
 
     @Query("SELECT * FROM sensor_table WHERE sensor_id = :sensorId")
-    fun getSensorBySensorId(key: (SensorData) -> Int, sensorId: Long) : Flow<SensorData>
+    fun getSensorBySensorId(key: String, sensorId: Long) : Flow<SensorData>
 }
