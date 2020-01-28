730a392822f21cd9bda0ba2e217c73590d9bbdd4
vdtyson <versilistyson@gmail.com>
2020-01-27 21:28:08 -0500

    - Built SensorLocalDataSourceImpl and SensorRemoteDataSourceImpl

---
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/util/HashcodeUtil.kt
@@ -0,0 +1,9 @@
+package com.versilistyson.welldone.data.util
+
+fun <T: Any> generateKeys(objs: List<T>): List<Int> {
+    val hashCodesForObjects = mutableListOf<Int>()
+    objs.forEach {
+        hashCodesForObjects.add(it.hashCode())
+    }
+    return hashCodesForObjects
+}
