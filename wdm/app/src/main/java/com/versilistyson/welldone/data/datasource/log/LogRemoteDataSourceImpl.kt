275f3ca0259b16ccb7c3ecc68328b22850987172
vdtyson <versilistyson@gmail.com>
2020-02-03 20:07:18 -0500

    Add Log Remote Data source Implementation

--- wdm/app/src/main/java/com/versilistyson/welldone/data/datasource/log/LogRemoteDataSourceImpl.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/data/datasource/log/LogRemoteDataSourceImpl.kt
@@ -4,6 +4,18 @@ import com.versilistyson.welldone.data.api.log.LogApi
 import com.versilistyson.welldone.domain.framework.datasource.log.LogRemoteDataSource
 import javax.inject.Inject
 
-class LogRemoteDataSourceImpl @Inject constructor(logApi: LogApi): LogRemoteDataSource {
+class LogRemoteDataSourceImpl @Inject constructor(private val logApi: LogApi): LogRemoteDataSource {
+    override suspend fun getLogsBySensorId(sensorId: Long) =
+        logApi.getLogs(sensorId)
+
+    override suspend fun addNewLog(logToAdd: LogApi.Dto.Log) =
+        logApi.addLog(logToAdd)
+
+    override suspend fun updateLog(logId: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteLogById(logId: Long) =
+        logApi.deleteLog(logId)
 
 }
--- wdm/app/src/main/java/com/versilistyson/welldone/domain/framework/datasource/log/LogRemoteDataSource.kt
+++ wdm/app/src/main/java/com/versilistyson/welldone/domain/framework/datasource/log/LogRemoteDataSource.kt
@@ -1,6 +1,12 @@
 package com.versilistyson.welldone.domain.framework.datasource.log
 
+import com.versilistyson.welldone.data.api.log.LogApi
 import com.versilistyson.welldone.domain.framework.datasource.BaseDataSource
+import retrofit2.Response
 
 interface LogRemoteDataSource: BaseDataSource {
+    suspend fun getLogsBySensorId(sensorId: Long): Response<List<LogApi.Dto.Log>>
+    suspend fun addNewLog(logToAdd: LogApi.Dto.Log): Response<Any>
+    suspend fun updateLog(logId: Long)
+    suspend fun deleteLogById(logId: Long)
 }
